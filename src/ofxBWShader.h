b3fb558d36e061d0641b436d22dd8e7b05e0a44f
erik430 <erik.yeung@430.com.hk>
2014-09-03 17:54:26 +0800

    first commit

---
+++ src/ofxBWShader.h
@@ -0,0 +1,77 @@
+//
+//  ofxHueShader.h
+//
+//
+//  Created by erikccoder on 9/3/14.
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+class ofxBWShader
+{
+private:
+    
+    ofShader shader;
+    
+    void setup()
+    {
+        
+#define STRINGIFY(x) #x
+        string HueShaderSrc = STRINGIFY(
+                                        
+                uniform sampler2DRect inputTexture;
+				void main() 
+				{
+					vec4 normalColor = texture2D(inputTexture, gl_TexCoord[0].xy);
+					float gray = 0.299*normalColor.r + 0.587*normalColor.g + 0.114*normalColor.b;
+					gl_FragColor = vec4(gray, gray, gray, normalColor.a); 
+				}
+);        
+        shader.setupShaderFromSource(GL_FRAGMENT_SHADER, HueShaderSrc);
+        shader.linkProgram();
+#undef STRINGIFY
+        
+    }
+    
+public:
+    
+    ofxBWShader()
+    {
+        setup();
+    }
+    ~ofxBWShader()
+    {
+        cout << "~ofxBWShader()" << endl;
+    }
+    
+    void begin()
+    {
+        shader.begin();
+// 		shader.setUniformTexture("inputTexture", tex.getTextureReference(), 0);
+    }
+    
+    void end(){
+        shader.end();
+    }
+    
+    
+    static void beginShader(float _hue){
+        instance()->begin(_hue);
+    }
+    static void endShader(){
+        instance()->end();
+    }
+    
+    static ofPtr<ofxBWShader> instance()
+    {
+        
+        static ofPtr<ofxBWShader>factory = ofPtr<ofxBWShader>(new ofxBWShader());
+        return factory;
+    }
+    
+};
+
+/* EOF */
